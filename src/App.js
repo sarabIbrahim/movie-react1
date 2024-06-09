72abe93fa5636f51589e65f54d28392295520fe1
Milad A. Kareem <42098710+milad-a-kareem@users.noreply.github.com>
2024-06-09 21:50:14 +0300

    Update App.js
    convert to https

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchIcon from './search.svg';
 import MovieCard from "./MovieCard";
 
 //4026541b
-const API_URL="http://www.omdbapi.com?apikey=4026541b";
+const API_URL="https://www.omdbapi.com?apikey=4026541b";
 
 
 
